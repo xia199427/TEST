8768772f75f1454dbac36523114ec41037ac8fba
liveangel <jiangshu.social@gmail.com>
2015-05-12 06:46:40 +0800

    change SDK from 17 to 21

--- app/src/main/java/com/example/liveangel/test/Main.java
+++ app/src/main/java/com/example/liveangel/test/Main.java
@@ -19,7 +19,7 @@ public class Main extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        //AA
+        //AAA
         return true;
     }
 
